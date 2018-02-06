6a6d7c181090516a57475c980e2b2ebd3dd76e39
darnosls <darnoslima@gmail.com>
2018-02-06 18:09:44 -0200

    final

--- snippets/views.py
+++ snippets/views.py
@@ -1,23 +1,32 @@
 # from rest_framework.renderers import JSONRenderer
 # from rest_framework.parsers import JSONParser
-# from snippets.models import Snippet
-# from snippets.serializers import SnippetSerializer
-# from django.http import HttpResponse, JsonResponse
 from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+# from django.http import HttpResponse, JsonResponse
+# from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
-# from django.http import Http404 # part3
-# from rest_framework.views import APIView # part3
-# from rest_framework.response import Response # part3
-# from rest_framework import status # part3
+# # from django.http import Http404 # part3
+# # from rest_framework.views import APIView # part3
+# # from rest_framework.response import Response # part3
+# # from rest_framework import status # part3
 
-# from rest_framework import mixins # part 3 - final
-from rest_framework import generics
+# # from rest_framework import mixins # part 3 - final
+# from rest_framework import generics
 
-# from django.views.decorators.csrf import csrf_exempt
-# from rest_framework.decorators import api_view
+# # from django.views.decorators.csrf import csrf_exempt
+# # from rest_framework.decorators import api_view
 from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework import renderers
+# from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+from rest_framework import viewsets
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+
 
 # @csrf_exempt # part 1
 # @api_view(['GET', 'POST']) # part 2
@@ -26,16 +35,34 @@ from django.contrib.auth.models import User
 # class SnippetList(mixins.ListModelMixin,
 #                   mixins.CreateModelMixin,
 #                   generics.GenericAPIView): # part 3 - final
-class SnippetList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    # """
-    # List all code snippets, or create a new snippet.
-    # """
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+# class SnippetHighlight(generics.GenericAPIView):
+#     queryset = Snippet.objects.all()
+#     renderer_classes = (renderers.StaticHTMLRenderer,)
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+#     def get(self, request, *args, **kwargs):
+#         snippet = self.get_object()
+#         return Response(snippet.highlighted)
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+
+
+# class SnippetList(generics.ListCreateAPIView):
+#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+#     # """
+#     # List all code snippets, or create a new snippet.
+#     # """
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
+
+#     def perform_create(self, serializer):
+#         serializer.save(owner=self.request.user)
 
     # # if request.method == 'GET':
     # # def get(self, request, format=None): # part 3
@@ -68,14 +95,14 @@ class SnippetList(generics.ListCreateAPIView):
 #                     mixins.UpdateModelMixin,
 #                     mixins.DestroyModelMixin,
 #                     generics.GenericAPIView): # part 3 - final
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly,)
-    # """
-    # Retrieve, update or delete a code snippet.
-    # """
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+# class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+#                           IsOwnerOrReadOnly,)
+#     # """
+#     # Retrieve, update or delete a code snippet.
+#     # """
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
 
     # def get_object(self, pk):
     #     try:
@@ -122,10 +149,37 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     #     return self.destroy(request, *args, **kwargs)
 
 
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# class UserList(generics.ListAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
 
-class UserDetail(generics.RetrieveAPIView):
+# class UserDetail(generics.RetrieveAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -72,6 +72,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tutorial.wsgi.application'
 
+# pagination
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
